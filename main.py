a83f8dd506e655f812999a328bb62d2074c4c152
Kartik Neti <kartikprajak@gmail.com>
2023-11-03 23:09:22 +0530

    added few more html files

--- main.py
+++ main.py
@@ -8,20 +8,28 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
-@app.route('/info')
-def info():
-    return "Puppies are cute"
+@app.route('/baseo')
+def hobaseome():
+    return render_template("base.html")
 
-@app.route('/puppy_latin/<name>')
-def name(name):
-    pupname=''
-    if name[-1] == 'y':
-        pupname = name[:-1] + 'ful'
-    else:
-        pupname= name+'y'
+@app.route('/homeo')
+def homeoe():
+    return render_template("home.html")
+
+# @app.route('/info')
+# def info():
+#     return "Puppies are cute"
+
+# @app.route('/puppy_latin/<name>')
+# def name(name):
+#     pupname=''
+#     if name[-1] == 'y':
+#         pupname = name[:-1] + 'ful'
+#     else:
+#         pupname= name+'y'
         
         
-    return "{} Puppies are cute".format(pupname.upper())
+#     return "{} Puppies are cute".format(pupname.upper())
 
 
 if __name__ == '__main__':
